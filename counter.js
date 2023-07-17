7a8539111187d3bef99357aac0c31543aca3dc29
Fajar Tri Haryanto <ftfajar5@gmail.com>
2023-07-18 00:45:08 +0700

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,20 @@
+let counter = 0;
+
+const increment = () => {
+  counter++;
+  document.querySelector(".counter-value").textContent = counter;
+};
+
+const decrement = () => {
+  counter--;
+  document.querySelector(".counter-value").textContent = counter;
+};
+
+const reset = () => {
+  counter = 0;
+  document.querySelector(".counter-value").textContent = counter;
+};
+
+document.querySelector("#increment").addEventListener("click", increment);
+document.querySelector("#decrement").addEventListener("click", decrement);
+document.querySelector("#reset").addEventListener("click", reset);
