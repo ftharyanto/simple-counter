f9bea34af904ed54e815150d831f0a5b87282d7d
Fajar Tri Haryanto <ftfajar5@gmail.com>
2023-07-29 17:28:33 +0700

    add stopwatch

--- counter.js
+++ counter.js
@@ -1,8 +1,12 @@
 let counter = 0;
+let start_time = Date.now();
 
 const increment = () => {
   counter++;
   document.querySelector(".counter-value").textContent = counter;
+  let current_time = Date.now();
+  let elapsed_time = current_time - start_time;
+  document.querySelector(".stopwatch").textContent = elapsed_time;
 };
 
 const decrement = () => {
@@ -12,10 +16,10 @@ const decrement = () => {
 
 const reset = () => {
   counter = 0;
+  start_time = Date.now();
   document.querySelector(".counter-value").textContent = counter;
+  document.querySelector(".stopwatch").textContent = "";
 };
-
-
 
 document.querySelector("#increment").addEventListener("click", increment);
 document.querySelector(".counter-value").addEventListener("click", increment);
